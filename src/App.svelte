a5950338723ad07558cd0cac770fb131c3bad063
spuckhafte <rakshit240405@gmail.com>
2024-12-29 19:36:14 +0530

    fix: unexisting import

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import About from "./lib/About.svelte";
     import Navbar from "./lib/Navbar.svelte";
-    import Skills from "./lib/Skills.svelte";
 
 </script>
 <main class="font-main w-screen h-screen overflow-x-hidden">
