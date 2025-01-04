35c3417bbf4b45b879c8bad2fccfb5a879e81a59
spuckhafte <rakshit240405@gmail.com>
2025-01-04 15:07:05 +0530

    chore: redesign navbar

--- src/lib/Experience.svelte
+++ src/lib/Experience.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import type { Action } from "svelte/action";
     import { Xps } from "./util/helpers";
     import XpCard from "./util/XpCard.svelte";
 
@@ -7,7 +6,7 @@
 
 </script>
 
-<main class="mt-20 py-5 md:pb-10 px-[1rem] md:px-[2.5rem] xl:px-[7rem] border bg-black text-white" id="experience">
+<main class="mt-20 py-5 md:pb-10 px-[1rem] md:px-[2.5rem] xl:px-[7rem] bg-black text-white" id="experience">
     <div>
         <div class="text-center text-xl py-5">My <b>Experience</b></div>
         <div class="flex flex-col gap-4 py-4">
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -33,7 +33,8 @@
 
 </script>
 
-<nav class="absolute w-full py-5 flex *:w-full justify-center items-center z-50 bg-white xl:px-[5rem] px-[1rem] ">
+<nav class="absolute w-full py-5 flex *:w-full justify-center items-center z-50 bg-white xl:px-[5rem] px-[1rem] shadow-lg">
+    
     <div class="flex items-center font-extrabold">
         <img src="/logo.png" alt="" width="40px" height="40px" fetchpriority="high"/>
         <div class="w-fit cursor-pointer transform translate-x-[-9px]">akshit</div>
@@ -58,16 +59,16 @@
 
 {#if screenWidth <= MEDIUM_BP}
     <div class={
-        "absolute z-40 flex flex-col border justify-center items-center gap-3 p-2.5 bg-black text-white w-full rounded-es-2xl rounded-ee-2xl transform " +
+        "absolute z-40 flex backdrop-blur-2xl bg-gradient-to-r from-white/80 to-white/30 flex-col justify-center items-center gap-3 p-2.5 w-full rounded-es-lg rounded-ee-lg transform text-black shadow-lg " +
             (showNavLinks ? "translate-y-[29.5%]" : "translate-y-[-100%]") + 
-            " transition-transform ease-in-out"
+            " transition-transform ease-in-out duration-300"
     }>
         
         <div class="*:font-[600] *:cursor-pointer *:text-center flex flex-col gap-3 py-3">
             {@render navlinks()}
         </div>
         <div class="pb-3">
-            <Button text="Resume" icon={faFileDownload} link="/resume.pdf" invert />
+            <Button text="Resume" icon={faFileDownload} link="/resume.pdf" />
         </div>
     </div>
 {/if}
