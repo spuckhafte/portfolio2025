aed71e5995299d13f71d6c62b09e9ff68327cefe
spuckhafte <rakshit240405@gmail.com>
2025-01-05 19:49:01 +0530

    chore: paddings

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -12,7 +12,7 @@
     "flex flex-col px-[1rem] md:px-[2.5rem] xl:px-[5rem] py-10 pt-10 bg-gradient-to-br from-black to-black/85 text-white"
 }>
     <div class="text-center text-xl pb-10"><b>Contact</b> Me</div>
-    <div class="flex flex-col gap-8 sm:flex-row sm:justify-between sm:gap-10 sm:items-center lg:gap-20 xl:gap-24">
+    <div class="flex flex-col gap-9 sm:flex-row sm:justify-between sm:gap-10 sm:items-center lg:gap-20 xl:gap-24">
         <div class="w-full">
             <form class="flex flex-col gap-3" id="user-msg" onsubmit={handleFormSubmit}>
                 <div>
@@ -40,7 +40,7 @@
                 </div>
             </form>
         </div>
-        <div class="w-full flex flex-col gap-5 sm:pb-14">
+        <div class="w-full flex flex-col gap-5 pb-3 sm:pb-14">
             <div class="text-3xl font-bold">
                 Let's talk for Something special
             </div>
