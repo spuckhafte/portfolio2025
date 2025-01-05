3114a0649b2940f7dca98612c90d8ad492a6d79b
spuckhafte <rakshit240405@gmail.com>
2025-01-05 19:31:57 +0530

    fix: footer sm padding

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,4 @@
-<main class="flex justify-between items-center py-3 px-3 sm:px-6 md:px-10 xl:px-20">
+<main class="flex justify-between items-center py-3 px-[1rem] sm:px-6 md:px-10 xl:px-20">
     <div class="flex items-center">
         <img src="/logo.png" class="transform translate-x-[-12px]" alt="icon" width="40px" height="40px" />
         <div class="text-sm font-bold transform translate-x-[-21px]">akshit</div>
