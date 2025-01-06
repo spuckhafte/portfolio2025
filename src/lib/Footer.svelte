933cec8f9ec9cbb6f810ca72135c067d77552a7b
spuckhafte <rakshit240405@gmail.com>
2025-01-06 12:39:39 +0530

    chore: footer padding

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,4 @@
-<main class="flex justify-between items-center py-4 px-[1rem] sm:px-6 md:px-10 xl:px-20" id="contact">
+<main class="flex justify-between items-center py-6 px-[1rem] sm:px-6 md:px-10 xl:px-20" id="contact">
     <div class="flex items-center">
         <div class="text-sm font-bold mygradient">spuckhafte</div>
     </div>
